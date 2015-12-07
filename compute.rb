dded97a5a15e0c6dbe7d86e7c2c69094c9301cb7
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2015-12-07 10:02:48 +0100

    Dodanie instrukcji porządkującej dane przed wydrukiem

--- compute.rb
+++ compute.rb
@@ -1,6 +1,7 @@
 require hermite
 
-; Dane do obliczeń: n, x, y, z
+# Dane do obliczeń: n, x, y, z
+# Przygotowanie danych do obliczeń 
 t = []
 t.pusb([])
 for x0 in x
@@ -8,6 +9,7 @@ for x0 in x
   t[0].push(x0)
 end for
 
+# Pętla obliczeniowa
 while last(t).length >1
   lt = last(t)
   nt = []
@@ -19,5 +21,12 @@ while last(t).length >1
   end for
   t.push(nt)
 end while
+
+# Przygotowanie danych do wydruku
+p = []
+for n in f
+  p.push(n[0])
+end for
+f = f[0]
 
 require printer

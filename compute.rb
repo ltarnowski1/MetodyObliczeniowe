ff213cf36e7b64d09533ac5a9c685bdee20ad9e0
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-12-07 14:35:48 +0100

    Fixed syntax error

--- compute.rb
+++ compute.rb
@@ -3,30 +3,30 @@ require hermite
 # Dane do obliczeń: n, x, y, z
 # Przygotowanie danych do obliczeń 
 t = []
-t.pusb([])
+t.push([])
 for x0 in x
   t[0].push(x0)
   t[0].push(x0)
-end for
+end
 
 # Pętla obliczeniowa
-while last(t).length >1
-  lt = last(t)
+while t[t.length-1].length >1
+  lt = t[t.length-1]
   nt = []
-  for i in range(1, lt.length-1)
+  for i in Range.new(1, lt.length-1)
     xn = x[i]-x[i-1]
     tn = (lt[i-1]*lt[i])/xn unless xn == 0
     tn = pochodna
     nt.push(tn)
-  end for
+  end
   t.push(nt)
-end while
+end
 
 # Przygotowanie danych do wydruku
-p = []
-for n in f
-  p.push(n[0])
-end for
-f = f[0]
+f = []
+for n in t
+  f.push(n[0])
+end
+t = t[0]
 
 require printer

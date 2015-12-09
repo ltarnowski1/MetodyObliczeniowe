0eb2c408264c4bfa8092281af05009dd0ea91f59
ltarnowski1 <ltarnowski@sigma.ug.edu.pl>
2015-12-09 17:32:48 +0100

    bebechy Hermita

--- Hermit.rb
+++ Hermit.rb
@@ -5,9 +5,12 @@ y = [1,2,3]
 z = [1,1,0]
 Rozm = t.length
 tab = Array.new(Rozm, Array.new(Rozm))
-
+wynik = []
 
 for i in 0...Rozm
+  if (i==0)
+  wynik.push(ft[i].to_f)
+  end
   tab[0][i] = ft[i].to_f
 end
 
@@ -26,9 +29,15 @@ for i in 1...Rozm
   g=g+1
   for j in 0...Rozm-g
     if (t[j+g]-t[j]) == 0
+      if (j==0)
+       wynik.push(z[t[j]].to_f)
+      end
       tab[i][j]=z[t[j]].to_f
       print " #{tab[i][j]}  |"
     else
+      if (j==0)
+       wynik.push(((tab[i-1][j+1]-tab[i-1][j])/(t[j+g]-t[j])).to_f)
+      end
       tab[i][j]=(tab[i-1][j+1]-tab[i-1][j])/(t[j+g]-t[j]).to_f
       if tab[i][j] < 0
         print " #{tab[i][j]} |"
@@ -40,5 +49,4 @@ for i in 1...Rozm
   puts
 end
 
-
-
+print wynik
